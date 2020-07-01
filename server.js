bfa2b15e98f0edda78a19ceb065d4827685ae116
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-07-01 09:47:56 -0500

    Pushing new server.js

--- server.js
+++ server.js
@@ -3,26 +3,26 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
 
 const app = express();
 
 app.use(logger("dev"));
 
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user1:password1@ds063899.mlab.com:63899/heroku_1h4z6mtx";
+mongoose.connect(MONGODB_URI);
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-// routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`Server listening on: http://localhost:${PORT}`);
 });
