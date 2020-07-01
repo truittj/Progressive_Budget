44d8c82b0e7ebd5e1f1be262f5987e9b2acbfc2b
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-07-01 17:21:32 -0500

    fixed scoping issues, everything seems to be working. Pushing to heroku

--- public/index.js
+++ public/index.js
@@ -1,13 +1,3 @@
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/service-worker.js')
-//       .then((reg) => {
-//         console.log('Service worker registered.', reg);
-//       });
-//   });
-// }
-
-
 let transactions = [];
 let myChart;
 
@@ -122,8 +112,8 @@ function sendTransaction(isAdding) {
   populateTable();
   populateTotal();
   
-   // also send to server
-   fetch("/api/transaction", {
+  // also send to server
+  fetch("/api/transaction", {
     method: "POST",
     body: JSON.stringify(transaction),
     headers: {
